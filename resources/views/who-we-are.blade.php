b73c454d392fa4addcc741549b6c423b3fd1b630
skiotto93 <fabio.forenza@hotmail.com>
2023-01-13 22:31:16 +0100

    Aggiunto who we are.blade.php

---
+++ resources/views/who-we-are.blade.php
@@ -0,0 +1,23 @@
+<!-- Style -->
+<style>
+    h1 {
+        text-align: center;
+        color: red;
+    }
+    h1:hover {
+        color: green;
+    }
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Who we are</title>
+</head>
+<body>
+    <h1>Chi siamo</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/homepage', function () {
     return view('homepage');
+});
+
+Route::get('/who-we-are', function () {
+    return view('who-we-are');
 });
